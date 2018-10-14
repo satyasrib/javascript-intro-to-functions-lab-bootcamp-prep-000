2ffcd8478468002d7ff7198c86bcfd187a673189
Satyasri Varaganti <satyasrib@gmail.com>
2018-10-14 06:20:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,3 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string) {
-  console.log(string.toLowerCase, string.toUpperCase, string."I love you, Grandma.")
-}
