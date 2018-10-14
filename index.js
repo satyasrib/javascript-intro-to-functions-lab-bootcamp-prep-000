542215a5c65c1bd71f3eb812db5c596a78d1bbc3
Satyasri Varaganti <satyasrib@gmail.com>
2018-10-14 05:37:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log("string.hellotoUpperCase")
+  console.log("spy.hello")
+  return string.toUpperCase()
 }
