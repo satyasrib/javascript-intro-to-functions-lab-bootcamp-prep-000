7192c4a976cb62c16a8d93102b5485cbcc65d768
Satyasri Varaganti <satyasrib@gmail.com>
2018-10-14 03:18:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function logShout(hello) {
+function logShout(HELLO) {
   console.log('hello')
   return `$its one argument in all caps`
 }
