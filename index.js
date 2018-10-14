859bd43496b0c98378a5b60b79e0d775be12411a
Satyasri Varaganti <satyasrib@gmail.com>
2018-10-14 06:47:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function sayHiToGrandma(string) {
 } if (string.toUpperCase() === string ) {
  return “YES INDEED!”
 } if (string === `I love you, Grandma.`) {
- return “I love you, too.”
-}}
+ return “I love you, too.”}
+ }
